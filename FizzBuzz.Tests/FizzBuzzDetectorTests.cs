058efb8d23719d4b746e9d23c4ce4e1e97f362c7
mazing74 <mezog39@gmail.com>
2026-07-24 00:38:21 +0300

    Add unit test for full sample input from spec

--- FizzBuzz.Tests/FizzBuzzDetectorTests.cs
+++ FizzBuzz.Tests/FizzBuzzDetectorTests.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-using FizzBuzz;
-
-namespace FizzBuzz.Tests
+﻿namespace FizzBuzz.Tests
 {
     public class FizzBuzzDetectorTests
     {
+        private readonly FizzBuzzDetector _detector = new();
+
+
+        [Fact]
+        public void GetOverlappings_Should_ReturnZeroCount_WhenNoWordIsReplaced()
+        {
+            string input = "Welcome Everyone";
+
+            var result = _detector.getOverlappings(input);
+
+            Assert.Equal("Welcome Everyone", result.Output);
+            Assert.Equal(0, result.Count);
+        }
+        [Fact]
+        public void GetOverlappings_Should_ReturnExpectedOutput_ForSampleInput()
+        {
+            string input = @"Mary had a little lamb Little lamb, little lamb Mary had a little lamb It's fleece was white as snow";
+
+            var result = _detector.getOverlappings(input);
+
+            string expected = @"Mary had Fizz little Buzz Fizz lamb, little Fizz Buzz had Fizz little lamb FizzBuzz fleece was Fizz as Buzz";
+
+            Assert.Equal(expected, result.Output);
+            Assert.Equal(9, result.Count);
+        }
     }
 }
